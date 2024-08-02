3f50a0940e096351969e64346ac9713f5c0c56d4
jordy verastegui dominguez <jordy.verastegui@alarcongroup.us>
2024-08-01 23:12:41 -0500

    .

---
+++ ruta.mjs
@@ -0,0 +1,5 @@
+import path from 'path';
+const __filename = new URL(import.meta.url).pathname;
+const __dirname = path.dirname(__filename);
+
+console.log(`La ruta del directorio actual es: ${__dirname}`);
